f6368bc652c08c62fbc3679c2624a844155a8852
Tom <35183444+bedax@users.noreply.github.com>
2019-04-23 08:53:15 +0100

    remapping capslock to alt_l instead of shift_l

--- home/.textadept/init.lua
+++ home/.textadept/init.lua
@@ -1,48 +1,37 @@
 -- todo:
 
--- ctrl+alt+h ignore case
--- when tabs are enabled disable mru on close
--- make sure the active view is a minimum of 80 wide
--- ctrl+h should work with selections, and then deselect
--- find_inc ctrl+h should highlight the content of find_inc input
--- highlight search matches while searching like vim's *
--- in the filtered list pgup and pgdn should work
--- remove the file type from the status bar
--- don't allow a buffer to be put in the background if it's unsaved; prompt to save
-   -- or at least notify in the status bar if there's an unsaved buffer in the background
--- show an 'unsaved' indicator on the left of the status bar
-   -- one for each view; -*---, hide if ----
-
--- ctrl+f should use the word under the cursor, it should highlight it (replacing ctrl+h)
-   -- and when enter is pressed it should clear highlights and rehighlight the entered search
-      -- https://foicica.com/wiki/goto-nearest-occurrence
-      -- or: findsetword at: https://foicica.com/wiki/gmc
-      -- or: https://github.com/gabdub/ta-tweaks/wiki/goto_nearest-module
-
--- word counter; https://foicica.com/wiki/stats
--- the first item of a filteredlist should be active at first for selecting and the text box should be active always for text input
--- if a filteredlist is showing "bin/script.py" then "script bin" should find it, not just "bin script"
-
--- https://github.com/lundmark/textadept_swm
-   -- or use findsetword at: https://foicica.com/wiki/gmc
-   -- or findall or multiedit at: https://bitbucket.org/SirAlaran/ta-common
-   -- Ctrl+Alt+Shift+H
-
 -- ensure non atomic save
 -- ensure consistent line endings
 -- detect tab width and type from file
 -- detect eol type from file
 
+-- in the filteredlists e.g. "bin/script.py" should be found by "script bin" as well as "bin script"
+-- the list of a filteredlist should always be active for the up and down arrow keys, the page keys, and enter, and the text box should always be active for all other keys
+
+-- search dialogs (find incremental, etc) should start with the current selection or the word under the cursor in its input box
+   -- highlight the input box content when launching the search dialog (replacing ctrl+h)
+      -- when enter is pressed (or ctrl+h), clear highlights and rehighlight the entered text
+         -- https://foicica.com/wiki/goto-nearest-occurrence
+
+-- show an 'unsaved' indicator on the left of the status bar
+   -- notify in red in the status bar if there's an unsaved buffer in the background
+   -- one for each view, hide if all saved
+
+-- ctrl+alt+h ignore case
+-- ctrl+h should work with selections, and then deselect
+-- make sure the active view is always a minimum of [80] wide
+-- when tabs are enabled disable mru on close
+
+-- https://foicica.com/wiki/stats
 -- https://foicica.com/wiki/export
 -- https://foicica.com/wiki/filediff
+-- https://foicica.com/wiki/textadept-viewer
 -- https://bitbucket.org/SirAlaran/ta-common/src
-
 -- https://github.com/editorconfig/editorconfig-textadept
 -- https://medium.com/@a_baez/beautify-textadept-87a0c6e384a8
--- https://foicica.com/wiki/textadept-viewer
 -- https://medium.com/@a_baez/a-textadept-module-db906f195195
--- https://foicica.com/wiki/line-number-margin
 -- https://foicica.com/wiki/distraction-free-mode
+-- https://foicica.com/wiki/line-number-margin
 
 
 require("theme")
