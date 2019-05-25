c203916b8ae1ba073ddd7d8e58802754688e656f
Tom <35183444+bedax@users.noreply.github.com>
2019-05-25 19:33:11 +0100

    - fixed firefox/user.js to stop codepen from not working - fixed firefox/user.js to make the clipboard work again - updated firefox/user.js with privacytools.io's updates

--- home/.mozilla/firefox/user.js
+++ home/.mozilla/firefox/user.js
@@ -6,20 +6,24 @@ user_pref("devtools.serviceWorkers.testing.enabled", true);
 user_pref("accessibility.typeaheadfind.enablesound", false);
 
 // from: https://www.privacytools.io/
-// also see: https://github.com/ghacksuserjs/ghacks-user.js/
-user_pref("privacy.firstparty.isolate", true);
-user_pref("privacy.resistFingerprinting", true);
-user_pref("privacy.trackingprotection.enabled", false);
 user_pref("browser.safebrowsing.malware.enabled", false);
 user_pref("browser.safebrowsing.phishing.enabled", false);
 user_pref("browser.send_pings", false);
 user_pref("browser.urlbar.speculativeConnect.enabled", false);
 user_pref("dom.battery.enabled", false);
-user_pref("dom.event.clipboardevents.enabled", false);
+user_pref("extensions.blocklist.url", "https://blocklists.settings.services.mozilla.com/v1/blocklist/3/%20/%20/");
+user_pref("extensions.pocket.enabled", false);
 user_pref("geo.enabled", false);
+user_pref("media.eme.enabled", false);
+user_pref("media.gmp-widevinecdm.enabled", false);
 user_pref("media.navigator.enabled", false);
 user_pref("network.cookie.cookieBehavior", 1);
 user_pref("network.http.referer.trimmingPolicy", 2);
-user_pref("network.http.referer.XOriginPolicy", 2);
+user_pref("network.http.referer.XOriginPolicy", 1);
 user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 user_pref("network.IDN_show_punycode", true);
+user_pref("privacy.firstparty.isolate", true);
+user_pref("privacy.resistFingerprinting", true);
+user_pref("privacy.trackingprotection.enabled", false);
+user_pref("privacy.trackingprotection.cryptomining.enabled", true);
+user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
